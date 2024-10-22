0cfc64938cdca40d75a440779d02d2108c63c588
GnuTLam <GnuTLam>
2024-10-22 18:23:04 +0700

    test database

---
+++ library-be/src/controllers/testdb.js
@@ -0,0 +1,30 @@
+const Book = require('../models/book');
+const Author = require('../models/author');
+const Tag = require('../models/tag');
+
+const testDB = async () => {
+    try {
+        const newBook = await Book.create({ title: 'Sample Book', authorId: 1 });
+        console.log('oke tao thanh cong ', newBook);
+
+        const book = await Book.findByPk(1);
+        console.log('oke thay sach', book);
+
+
+        const newAuthor = await Author.create({ name: 'Sample Author' });
+        console.log('tao tacgia moi:', newAuthor);
+
+        const author = await Author.findByPk(1);
+        console.log('tim kiem tac gia:', author);
+
+        const tag = await Tag.findByPk(1);
+        console.log('Found Tag:', tag);
+
+
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
+
+testDB();
