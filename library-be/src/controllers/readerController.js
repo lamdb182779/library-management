353451a0f4aad5868958b2eb3b11d745ee694791
greensnake7 <gothlamnguyen@gmail.com>
2024-11-10 20:22:59 +0700

    fix bug

--- library-be/src/controllers/readerController.js
+++ library-be/src/controllers/readerController.js
@@ -1,4 +1,4 @@
-const { Reader, Book, BookReader, BookAuthor, BookTag, Publisher, Author, Tag } = require('../models');
+const { Reader, Book, BookReader, Publisher, Author, Tag } = require('../models');
 const { Op } = require('sequelize');
 
 const ReaderController = {
